0c6779212a34950f2b261f7bea669d7ec917c4e9
Inhumannn <thomas.pnbm@gmail.com>
2025-03-26 16:07:33 +0100

    feat: update signs component to display dynamic zodiac signs and dates

--- src/components/signes.jsx
+++ src/components/signes.jsx
@@ -8,11 +8,11 @@ function Signes() {
   return (
     <div className="flex flex-row gap-[40px] items-center pl-[10%]">
       <button className="border-t border-[#BFBFBF]" onClick={prev}>
-        <p className="text-start">Sagittaire </p>
+        <p className="text-start">{currentHoroscopePrev.signe}</p>
         <p>{currentHoroscopePrev.date}</p>
       </button>
       <button className="border-t border-[#BFBFBF]" onClick={next}>
-        <p className="text-start">Cancer</p>
+        <p className="text-start">{currentHoroscopeNext.signe}</p>
         <p>{currentHoroscopeNext.date}</p>
       </button>
     </div>
