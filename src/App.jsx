41bd776e31064ff56c6a7d2867f6bc2c6ca2e852
Inhumannn <thomas.pnbm@gmail.com>
2025-03-27 10:31:03 +0100

    feat: add Not Found page and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import './styles/home.css';
 import APropos from "./pages/apropos";
 import Contact from "./pages/contact";
+import Home from "./pages/home";
+import NotFound from "./pages/notfound";
+import './styles/home.css';
 
 function App(){
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/apropos" element={<APropos />} />
         <Route path="/contact" element={<Contact />} />
