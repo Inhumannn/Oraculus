7d20bc3f2b60ae6e67bbf1b121b051e39605e57f
Inhumannn <thomas.pnbm@gmail.com>
2025-03-27 10:25:56 +0100

    style(contact): improve contact page styling
    - Added responsive styling
    - Improved form layout
    - Updated text styling
    - Added placeholder text
    - Improved overall appearance

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,17 +5,17 @@ function Contact(){
   return(
     <div>
       <Header />
-      <main>
-        <section>
-            <h1>Contacter ORACULUS</h1>
-            <p>Nous sommes ravis que vous souhaitiez entrer en contact avec nous ! Chez Oraculus, nous sommes là pour répondre à toutes vos questions, suggestions ou préoccupations. Votre opinion compte, et nous sommes déterminés à vous offrir la meilleure expérience possible.</p>
-            <form action="#">
-                <input type="text" name="name" id="name" placeholder="blbal"/>
-                <input type="text" name="name" id="name"  />
-                <input type="email" name="mail" id="mail" />
-                <textarea name="" id="">blabla</textarea>
-                <input type="submit" value="" />
-            </form>
+      <main className="absolute left-[20%] grid grid-cols-[auto_60%]">
+        <section className="h-[598px] overflow-scroll">
+          <h1 className="text-[36px] pt-[100px] pb-[3px]">Contacter ORACULUS</h1>
+          <p className="text-[#808080]">Nous sommes ravis que vous souhaitiez entrer en contact avec nous ! Chez Oraculus, nous sommes là pour répondre à toutes vos questions, suggestions ou préoccupations. Votre opinion compte, et nous sommes déterminés à vous offrir la meilleure expérience possible.</p>
+          <form action="#" className="my-[50px] grid grid-cols-1 border border-[#808080] py-[100px] px-[60px] gap-[20px]">
+              <input type="text" name="name" id="name" placeholder="VOTRE PRENOM" className="border-b border-[#BFBFBF]"/>
+              <input type="text" name="name" id="name"  placeholder="VOTRE NOM" className="border-b border-[#BFBFBF]"/>
+              <input type="email" name="mail" id="mail" placeholder="VOTRE E-MAIL" className="border-b border-[#BFBFBF]"/>
+              <textarea name="" id="" placeholder="VOTRE MESSAGE" className="border-b border-[#BFBFBF]" />
+              <input type="submit" value="ENVOYER" className="text-[#8E8E8E] border border-[#BFBFBF] py-[20px]"/>
+          </form>
         </section>
         <Aside />
       </main>
